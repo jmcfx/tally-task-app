4dd9c5aee1094475afee3d3284c200572675921d
jmcfx <uispoj@gmail.com>
2025-09-16 23:31:23 +0100

    Fix : Clean imports

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:tally_task/main.dart';
+import 'package:tally_task/app/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
